7c284c142c1d81bf56dfa20f473dffc5e684a980
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2013-10-06 04:25:54 +0200

    Export preprocessor.Settings.

--- smc/mw/__init__.py
+++ smc/mw/__init__.py
@@ -8,5 +8,5 @@ from . mw_pre import mw_preParser as PreprocessorParser
 from . semantics import mwSemantics as Semantics
 from . semantics import SemanticsTracer
 from . preprocessor import mw_preSemantics as PreprocessorSemantics
-from . preprocessor import Preprocessor
+from . preprocessor import Preprocessor, Settings
 from . mediawiki import MediaWiki, mediawiki
