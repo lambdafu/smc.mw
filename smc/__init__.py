72f6c838c8466358138b53a7e5181cbd36b4d22a
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2014-07-26 14:14:52 +0200

    Handle namespace package a bit differently.

--- smc/__init__.py
+++ smc/__init__.py
@@ -1,5 +1,8 @@
 # Copyright 2013 semantics GmbH
 # Written by Marcus Brinkmann <m.brinkmann@semantics.de>
 
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
